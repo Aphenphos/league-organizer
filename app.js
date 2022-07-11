09b2cc9a3ad47348c110e10245d46b833db96116
Aphenphos <MaxwillWinters@gmail.com>
2022-07-11 15:29:55 -0400

    settting up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { getUser, signOut } from './services/auth-service.js';
 import { protectPage } from './utils.js';
 import createUser from './components/User.js';
-
+// displays teams
 // State
 let user = null;
 
